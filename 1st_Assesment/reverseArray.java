df477bfc5437a9a14df2b1c067c8b7bee24f2b2f
Piyash Rastogi <piyashrastogi11@gmail.com>
2021-11-14 13:00:45 +0530

    reverseArray updated

--- 1st_Assesment/reverseArray.java
+++ 1st_Assesment/reverseArray.java
@@ -1,3 +1,11 @@
+/*
+Take as input N, the size of array.
+Take N more inputs and store that in an array.
+Take as input M, a number.
+Write a recursive function which returns the last index at which M is found in the array and -1 if M is not found anywhere.
+Print the value returned.
+*/
+
 import java.util.Scanner;
 class reverseArray
 {
