c296c9f2093373810ee4174e7b04ee5d5c0e07ac
Piyash Rastogi <piyashrastogi11@gmail.com>
2021-11-14 12:58:39 +0530

    sorted array updated

--- 1st_Assesment/sortedArray.java
+++ 1st_Assesment/sortedArray.java
@@ -1,3 +1,8 @@
+/*Take as input N, the size of array. Take N more inputs and store that in an array.
+  Write a recursive function which returns true if the array is sorted and false otherwise.
+  Print the value returned.*/
+
+
 import java.util.*;
 public class sortedArray {
     public static boolean sortedArr(int[] arr,int end)
